f3d01461012b27320575c8e24b53b15c336d0860
Chanan Braunstein <chanan@bkbunch.com>
2018-12-17 15:27:29 -0700

    Trying to get the docs to work...

---
+++ docs/_content/BlazorPrettyCode/blazorPrettyCode.js
@@ -0,0 +1,8 @@
+﻿window.blazorPrettyCode = {
+    getAndHide: function (id) {
+        var elem = document.getElementById(id);
+        const text = elem.innerHTML;
+        elem.style.display = 'none';
+        return text;
+    }
+};
