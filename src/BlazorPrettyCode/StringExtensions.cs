340a0bb6c6311b9151e46e290c6a4c418e1e436d
Chanan Braunstein <chanan@bkbunch.com>
2019-08-06 08:11:01 -0600

    Doc updates

--- src/BlazorPrettyCode/StringExtensions.cs
+++ src/BlazorPrettyCode/StringExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BlazorPrettyCode
+﻿namespace BlazorPrettyCode
 {
     internal static class StringExtensions
     {
